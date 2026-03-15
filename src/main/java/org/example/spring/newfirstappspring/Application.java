1ae89f57697e2a93996daf9c1835ef096d016aa5
MrJerry <my_main.com>
2026-03-15 19:36:29 +0200

    F4_@Scopre, @PostConstruct, @PreDestry

--- src/main/java/org/example/spring/newfirstappspring/Application.java
+++ src/main/java/org/example/spring/newfirstappspring/Application.java
@@ -4,23 +4,17 @@ package org.example.spring.newfirstappspring;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-//        SpringApplication.run(NewFirstAppSpringApplication.class, args);
 
-        //IoC
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.example.spring.newfirstappspring");
-        //DI
-        Person person = context.getBean("person", Person.class);
-        person.startVehicleEngine();
+        Vehicle vehicle1 = context.getBean("car", Vehicle.class);
+        vehicle1.startEngine();
 
-        Parking parking = context.getBean("parking", Parking.class);
-        System.out.println(parking.getVehiclesList());
-        System.out.println(parking.getVehiclesMap());
         context.close();
+        System.out.println("Program is finished");
     }
 }
